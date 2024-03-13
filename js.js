3f445494a70b919b9dd047b9a92e14e6f425826e
Kevin Ukota <z1ppm4n@gmail.com>
2024-03-13 14:40:56 +0100

    Add and modify a button that prompts the user for number of squares per grid, removes the old and makes the new grid
    Add a button at the top of the grid that prompts the user for a number
    with a limit of 100, removes the current grid and creates a new grid with the
    user input number

--- js.js
+++ js.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const container = document.createElement("mainDiv");
+const button = document.createElement("button");
+button.textContent = "Grid Select";
 container.id = "main";
 document.addEventListener("DOMContentLoaded", function () {
-  document.body.appendChild(container);
+  document.body.append(button, container);
 });
 
 for (let i = 0; i < 256; i++) {
@@ -14,4 +16,37 @@ for (let i = 0; i < 256; i++) {
   square.addEventListener("mouseover", () => {
     square.style.backgroundColor = "black";
   });
+}
+
+button.addEventListener("click", () => {
+  let gridNum = prompt(
+    "Enter the number of squares for your grid (maximum of 100"
+  );
+  if (gridNum > 100) {
+    gridNum = prompt(
+      "Invalid number, enter a valid number of squares for your grid"
+    );
+    createGrid(gridNum);
+  }
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+  createGrid(gridNum);
+});
+
+function createGrid(number) {
+  const grid = number * number;
+  for (let i = 0; i < grid; i++) {
+    const square = document.createElement("div");
+    const dimension = 800 / number + "px";
+
+    container.appendChild(square);
+
+    square.style.width = dimension;
+    square.style.height = dimension;
+
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+  }
 }
